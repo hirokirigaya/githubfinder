75c44fd0f08269f84bdd00307808f27c22f5d78b
Hiro Kirigaya <privz286@gmail.com>
2022-05-17 15:38:46 -0300

    :iphone: Responsive

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,30 +4,35 @@ import './styles.css'
 import BtnTheme from '../../components/BtnTheme'
 import Github from '../../components/Images/Github'
 
-import {FiMoon as Moon, FiSun as Sun} from 'react-icons/fi'
-
+import { FiMoon as Moon, FiSun as Sun } from 'react-icons/fi'
 
 function index({ toggleTheme, theme }) {
   return (
     <section className="home-container">
       <div className="btn-theme">
-        <BtnTheme toggleTheme={toggleTheme} theme={theme === 'dark' ? <Sun/> : <Moon/>}/>
+        <BtnTheme
+          toggleTheme={toggleTheme}
+          theme={theme === 'dark' ? <Sun /> : <Moon />}
+        />
       </div>
-      <div className="container-finder">
-        <div className="title">
-          <h1>Github Finder</h1>
-          <p>
-            Enter path the user ex:<br/>
-            github.com/<span>hirokirigaya</span>
-          </p>
-        </div>
-        <div className="finder">
-            <input type="text" placeholder="hirokirigaya"/>
+      <div className="direction">
+        <div className="container-finder">
+          <div className="title">
+            <h1>Github Finder</h1>
+            <p>
+              Enter path the user ex:
+              <br />
+              github.com/<span>hirokirigaya</span>
+            </p>
+          </div>
+          <div className="finder">
+            <input type="text" placeholder="hirokirigaya" />
             <button>FIND</button>
+          </div>
         </div>
-      </div>
-      <div className="github-image">
-        <Github/>
+        <div className="github-image">
+          <Github />
+        </div>
       </div>
     </section>
   )
